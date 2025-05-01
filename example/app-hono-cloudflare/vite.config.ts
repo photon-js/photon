63699c20b549a717d339438ac6e1e4579943c162
Romuald Brillout <git@brillout.com>
2025-05-01 14:45:48 +0200

    comment plan

--- example/app-hono-cloudflare/vite.config.ts
+++ example/app-hono-cloudflare/vite.config.ts
@@ -1,4 +1,7 @@
 import { cloudflare } from '@photonjs/cloudflare/vite'
+/* The Vite plugin cloudflare() will be replaced by this:
+import cloudflare from '@photonjs/cloudflare'
+*/
 import { awesomeFramework } from 'awesome-framework/vite'
 import { defineConfig } from 'vite'
 
@@ -8,10 +11,13 @@ export default defineConfig(({ mode }) => {
       entry: {
         index: 'server.ts',
       },
+      /* The Vite plugin cloudflare() will be replaced by this:
+      target: cloudflare, // not needed when using @photonjs/auto
+      */
     },
     plugins: [
-      // Not needed when using @photonjs/auto
-      mode === 'cloudflare' && cloudflare(),
+      // Will be replaced with a photon.target setting
+      mode === 'cloudflare' && cloudflare(), // not needed when using @photonjs/auto
       awesomeFramework(),
     ],
   }
