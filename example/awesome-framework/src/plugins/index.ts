3de5cd322588cd3e634c9ab9dce278d76e9746c6
Romuald Brillout <git@brillout.com>
2025-05-01 13:25:35 +0200

    Revert "minor refactor: rename vite-plugin => awesome-framework"
    This reverts commit 825d06371fe00b8ef689dc1c7e11415181b3c328.

--- example/awesome-framework/src/plugins/index.ts
+++ example/awesome-framework/src/plugins/index.ts
@@ -1,6 +1,6 @@
 import { installPhoton } from '@photonjs/core/vite'
 import type { Plugin } from 'vite'
-import { awesomeFrameworkPlugin } from './awesome-framework.js'
+import { awesomeFrameworkPlugin } from './vite-plugin.js'
 
 export function awesomeFramework(): Plugin[] {
   return [awesomeFrameworkPlugin(), ...installPhoton('awesome-framework', { fullInstall: true })]
