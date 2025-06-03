2398494eff39287ea6b5c8797d0cc39763f36d54
Joël Charles <joel.charles91@gmail.com>
2025-06-03 12:48:34 +0200

    refactor: better Error management

--- packages/photonjs/src/api.ts
+++ packages/photonjs/src/api.ts
@@ -4,6 +4,14 @@ import './types.js'
 export { isPhotonMeta, type PhotonMeta } from './plugin/utils/entry.js'
 export { resolvePhotonConfig } from './validators/coerce.js'
 export { setPhotonHandler } from './api/setPhotonEntry.js'
+export {
+  PhotonError,
+  PhotonBugError,
+  PhotonUsageError,
+  PhotonConfigError,
+  PhotonRuntimeError,
+  PhotonDependencyError,
+} from './utils/assert.js'
 
 declare module 'vite' {
   interface UserConfig {
--- packages/photonjs/src/index.ts
+++ packages/photonjs/src/index.ts
@@ -1,1 +1,9 @@
 export * as api from './api.js'
+export {
+  PhotonError,
+  PhotonBugError,
+  PhotonUsageError,
+  PhotonConfigError,
+  PhotonRuntimeError,
+  PhotonDependencyError,
+} from './utils/assert.js'
