77866227ce0cba2407d87b55ca8500e92e4e53cb
Joël Charles <joel.charles91@gmail.com>
2025-06-04 18:52:58 +0200

    feat: new getPhotonServerEntryId util

--- packages/photonjs/src/api/api.ts
+++ packages/photonjs/src/api/api.ts
@@ -7,3 +7,13 @@ export function setPhotonHandler(pluginContext: PluginContext, fileName: string,
     id: asPhotonEntryId(entry.id, 'handler-entry'),
   }
 }
+
+export function getPhotonServerEntryId(
+  pluginContext: PluginContext,
+  opts?: { handlerId: string; condition: 'dev' | 'node' | 'edge' },
+) {
+  if (!opts) {
+    return pluginContext.environment.config.photon.server.id
+  }
+  return `${pluginContext.environment.config.photon.server.id}?photonCondition=${opts.condition}&photonHandlerId=${opts.handlerId}`
+}
