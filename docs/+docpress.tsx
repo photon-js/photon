ab1619d6ead22d4bb47a4f9c4fe27e63c5c7545c
Rom <git@brillout.com>
2025-06-20 10:07:07 +0200

    update docpress (#9)

---
+++ docs/+docpress.tsx
@@ -0,0 +1,24 @@
+export { config as default }
+
+import type { Config } from '@brillout/docpress'
+import logo from './assets/logo.svg'
+import { headings, headingsDetached, categories } from './headings'
+import { TopNavigation } from './TopNavigation'
+import React from 'react'
+
+const config: Config = {
+  name: 'Photon' as const,
+  version: '0.0.0',
+  url: 'photonjs.dev',
+  tagline: 'Any server, deployed anywhere.',
+  logo,
+
+  github: 'https://github.com/photon-js/photon' as const,
+
+  headings,
+  headingsDetached,
+  categories,
+
+  topNavigation: <TopNavigation />,
+  navMaxWidth: 1140,
+}
