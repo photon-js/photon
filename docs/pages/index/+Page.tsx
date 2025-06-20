1bfdfcdda90ac4b6e8e4d9226f9cd9907f5d9a1e
Romuald Brillout <git@brillout.com>
2025-06-20 11:41:51 +0200

    prune

--- docs/pages/index/+Page.tsx
+++ docs/pages/index/+Page.tsx
@@ -1,7 +1,7 @@
 export { Page }
 
 import React from 'react'
-import { LoremIpsum } from './LoremIpsum'
+import logoWithText from '../../assets/logo-with-text.svg'
 
 function Page() {
   return (
@@ -9,6 +9,8 @@ function Page() {
       <Block noMargin>
         <Header />
         <LoremIpsum />
+      </Block>
+      <Block>
         <LoremIpsum />
         <div style={{ height: 30 }} />
       </Block>
@@ -19,7 +21,7 @@ function Page() {
 function Header() {
   return (
     <>
-      <h1 style={{ textAlign: 'center', fontSize: '3.4em' }}>Next Generation Docs</h1>
+      <h1 style={{ textAlign: 'center', fontSize: '3.4em' }}><img src={logoWithText} height={50} /></h1>
     </>
   )
 }
@@ -36,6 +38,30 @@ function Block({ children, noMargin }: { children: React.ReactNode; noMargin?: t
       }}
     >
       <div style={{ maxWidth: 1000 }}>{children}</div>
+    </div>
+  )
+}
+
+function LoremIpsum() {
+  return (
+    <div>
+      <p>
+        Praesent <em>eu augue lacinia</em>, tincidunt purus nec, ultrices ante. Donec dolor felis, ornare vel augue
+        condimentum.
+      </p>
+      <p>
+        Mauris <code>foo</code> quis scelerisque erat.
+      </p>
+      <p>
+        <i>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</i>
+      </p>
+      <p>
+        <b>Curabitur</b> gravida urna id ligula volutpat dapibus. Integer accumsan dignissim efficitur. Sed mauris
+        tortor, lobortis at suscipit ac, ultricies eu nunc.
+      </p>
+      <p>
+        Sed molestie tempus &mdash; <b>elementum</b>.
+      </p>
     </div>
   )
 }
