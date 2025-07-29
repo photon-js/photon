6695f09f066982ca59ed2cb81df39f93288dcc54
Joël Charles <joel.charles91@gmail.com>
2025-07-29 14:43:07 +0200

    doc(example): comments

--- example/awesome-framework/src/vite/photonPlugin.ts
+++ example/awesome-framework/src/vite/photonPlugin.ts
@@ -3,13 +3,25 @@ import type { Plugin } from 'vite'
 
 export function photonPlugin(): Plugin[] {
   return installPhoton('awesome-framework', {
+    // Enables full installation of all Photon plugins
     fullInstall: true,
+
+    // Disables code-splitting functionality for testing purposes
+    codeSplitting: false,
+
+    // Always use those middlewares for all entries defined by Photon
     resolveMiddlewares() {
       return 'awesome-framework/universal-middleware'
     },
+
+    // Configuration for entry points used by Photon
     entries: {
       standalone: {
+        // Unique identifier for this entry point. Is resolved by Vite
         id: 'awesome-framework/standalone',
+
+        // Sets composition mode to 'isolated', preventing it from
+        // being composed with middlewares defined in `resolveMiddlewares`
         compositionMode: 'isolated',
       },
     },
