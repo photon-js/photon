30df278493ff6bfd7cc2bf3ab97c7432fde7f9c2
Joël Charles <joel.charles91@gmail.com>
2025-08-19 13:32:22 +0200

    chore: exclude most dependencies from optimizeDeps

--- packages/photonjs/src/plugin/plugins/commonConfig.ts
+++ packages/photonjs/src/plugin/plugins/commonConfig.ts
@@ -10,6 +10,33 @@ function commonConfig(): Plugin[] {
     singleton({
       name: 'photon:common-config',
 
+      config() {
+        return {
+          ssr: {
+            // From Vite doc:
+            // > If the dependency is small and is already valid ESM,
+            // > you can exclude it and let the browser load it directly.
+            optimizeDeps: {
+              exclude: [
+                'hono',
+                'h3',
+                'elysia',
+                '@universal-middleware/cloudflare',
+                '@universal-middleware/compress',
+                '@universal-middleware/core',
+                '@universal-middleware/elysia',
+                '@universal-middleware/express',
+                '@universal-middleware/fastify',
+                '@universal-middleware/h3',
+                '@universal-middleware/hattip',
+                '@universal-middleware/hono',
+                '@universal-middleware/sirv',
+              ],
+            },
+          },
+        }
+      },
+
       configEnvironment(name, config) {
         if (!config.consumer) {
           config.consumer = name === 'client' ? 'client' : 'server'
