6c8eda46cc444a99af67ba07fea9f30bfcaffc3a
Joël Charles <joel.charles91@gmail.com>
2025-09-11 13:02:29 +0200

    fix: properly compute `optimizeDeps`

--- packages/core/src/plugin/plugins/commonConfig.ts
+++ packages/core/src/plugin/plugins/commonConfig.ts
@@ -1,4 +1,6 @@
 import { defaultClientConditions, defaultExternalConditions, defaultServerConditions, type Plugin } from "vite";
+import type { Photon } from "../../types.js";
+import { resolvePhotonConfig } from "../../validators/coerce.js";
 import { SupportedServers } from "../../validators/validators.js";
 import { singleton } from "../utils/dedupe.js";
 import { isBun } from "../utils/isBun.js";
@@ -7,42 +9,63 @@ import { isDeno } from "../utils/isDeno.js";
 export { commonConfig };
 
 function commonConfig(): Plugin[] {
+  let resolvedPhotonConfig: Photon.ConfigResolved;
   return [
     singleton({
-      name: "photon:common-config",
+      name: "photon:optimize-deps",
 
-      config() {
+      config: {
+        order: "post",
+        handler(userConfig) {
+          resolvedPhotonConfig = resolvePhotonConfig(userConfig.photon);
+        },
+      },
+      configEnvironment(name, config) {
+        if (!config.consumer) {
+          config.consumer = name === "client" ? "client" : "server";
+        }
+
+        if (config.consumer !== "server") return;
         const servers = Object.keys(SupportedServers.enum);
 
+        // Photon can replace entries with virtual modules. If so, we need to ensure that `optimizeDeps.entries`
+        // still points to users entries so that Vite can crawl them
+        const entries = [resolvedPhotonConfig.server.id, ...resolvedPhotonConfig.entries.map((e) => e.id)]
+          .filter(Boolean)
+          .map((id) => id.replace(/^photon:(handler-entry|server-entry|server-config):/, ""));
+
         return {
-          ssr: {
-            // From Vite doc:
-            // > If the dependency is small and is already valid ESM,
-            // > you can exclude it and let the browser load it directly.
-            // When using cloudflare, if you stumble upon the following error, you probably needs to exclude some
-            // dependency from optimizeDeps: "There is a new version of the pre-bundle for [...]"
-            optimizeDeps: {
-              exclude: [
-                "hono",
-                "h3",
-                "srvx",
-                "elysia",
-                ...servers.map((s) => `@universal-middleware/${s}`),
-                "@universal-middleware/compress",
-                "@universal-middleware/core",
-                "@universal-middleware/sirv",
-                "@universal-middleware/cloudflare",
-                "@universal-middleware/vercel",
-                ...servers.map((s) => `@photonjs/${s}`),
-                "@photonjs/core",
-                "@photonjs/runtime",
-                "@photonjs/cloudflare",
-                "@photonjs/vercel",
-              ],
-            },
+          // From Vite doc:
+          // > If the dependency is small and is already valid ESM,
+          // > you can exclude it and let the browser load it directly.
+          // When using cloudflare, if you stumble upon the following error, you probably needs to exclude some
+          // dependency from optimizeDeps: "There is a new version of the pre-bundle for [...]"
+          optimizeDeps: {
+            entries,
+            include: ["@photonjs/core > @brillout/picocolors"],
+            exclude: [
+              "hono",
+              "h3",
+              "srvx",
+              "elysia",
+              ...servers.map((s) => `@universal-middleware/${s}`),
+              "@universal-middleware/compress",
+              "@universal-middleware/core",
+              "@universal-middleware/sirv",
+              "@universal-middleware/cloudflare",
+              "@universal-middleware/vercel",
+              ...servers.map((s) => `@photonjs/${s}`),
+              "@photonjs/core",
+              "@photonjs/runtime",
+              "@photonjs/cloudflare",
+              "@photonjs/vercel",
+            ],
           },
         };
       },
+    }),
+    singleton({
+      name: "photon:common-config",
 
       configEnvironment(name, config) {
         if (!config.consumer) {
