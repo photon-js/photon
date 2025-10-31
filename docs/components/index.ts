88dcae8d96239771ace8ac2d0ec039335c8c7f81
Romuald Brillout <git@brillout.com>
2025-10-31 12:40:15 +0100

    remove unused website code

--- docs/components/index.ts
+++ docs/components/index.ts
@@ -1,12 +1,1 @@
-// Export all reusable components
-
-export { ApiReference } from "./ApiReference";
-export { BetaWarning } from "./BetaWarning";
-export { CodeBlock } from "./CodeBlock";
-export { ExampleCard } from "./ExampleCard";
 export { FeatureGrid } from "./FeatureGrid";
-export { FrameworkComparison } from "./FrameworkComparison";
-export { InstallCommand } from "./InstallCommand";
-export { NextSteps } from "./NextSteps";
-export { PackageInfo } from "./PackageInfo";
-export { Tabs } from "./Tabs";
