3f5f12db201483151ab72b7f357aabedfcf51089
Joël Charles <joel.charles91@gmail.com>
2025-11-18 15:22:22 +0100

    feat: elysia

--- packages/elysia/src/serve-deno.ts
+++
@@ -1,8 +0,0 @@
-import { denoServe, type ServerOptionsBase } from "@photonjs/core/serve";
-import type { App as ElysiaApp } from "@universal-middleware/elysia";
-
-export function serve<App extends ElysiaApp>(app: App, options: ServerOptionsBase = {}) {
-  denoServe(options, app.fetch);
-
-  return app;
-}
