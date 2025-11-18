0617209a8f53242c639ac98dfdeaf88945acb0cf
Joël Charles <joel.charles91@gmail.com>
2025-11-18 19:08:05 +0100

    chore(hono): optimization

--- packages/hono/src/serve.ts
+++ packages/hono/src/serve.ts
@@ -1,22 +1,19 @@
 import type { IncomingMessage, ServerResponse } from "node:http";
-import type { Http2ServerRequest, Http2ServerResponse } from "node:http2";
 import { PhotonDependencyError } from "@photonjs/core/errors";
-import type { ServeReturn } from "@photonjs/core/serve";
+import type { NodeHandler, ServeReturn } from "@photonjs/core/serve";
 import type { App as HonoApp } from "@universal-middleware/hono";
 import type { MergedHonoServerOptions } from "./types.js";
 
 export function serve<App extends HonoApp>(app: App, options: MergedHonoServerOptions = {}): ServeReturn<App> {
   const exposeNodeHandler = Boolean(options.overrideGlobalObjects || options.autoCleanupIncoming || options.hostname);
 
-  let nodeHandler:
-    | undefined
-    | ((
-        incoming: IncomingMessage | Http2ServerRequest,
-        outgoing: ServerResponse | Http2ServerResponse,
-      ) => void | Promise<void>);
+  let nodeHandler: undefined | NodeHandler;
 
   if (exposeNodeHandler) {
-    nodeHandler = async (incoming, outgoing) => {
+    const honoHandler: NodeHandler = async (incoming, outgoing) => {
+      // quick access to `nodeHandler` if already overriden
+      if (nodeHandler && honoHandler !== nodeHandler)
+        return nodeHandler(incoming as IncomingMessage, outgoing as ServerResponse);
       try {
         // FIXME when built on userland, this should be included into bundle only if it is installed (falling back to dummy module?)
         const { getRequestListener } = await import("@hono/node-server");
@@ -28,6 +25,7 @@ export function serve<App extends HonoApp>(app: App, options: MergedHonoServerOp
         throw new PhotonDependencyError(`Missing @hono/node-server package. Add it to your dependencies`, { cause: e });
       }
     };
+    nodeHandler = honoHandler;
   }
 
   return {
