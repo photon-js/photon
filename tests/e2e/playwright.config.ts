182d7afc42b6357ff9016f41ea5d13948e514ccc
Joël Charles <joel.charles91@gmail.com>
2025-12-04 14:50:14 +0100

    chore: fix build

--- tests/e2e/playwright.config.ts
+++ tests/e2e/playwright.config.ts
@@ -30,6 +30,7 @@ export default defineConfig({
   testDir: "../../tests/e2e/tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
+  preserveOutput: "never",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
