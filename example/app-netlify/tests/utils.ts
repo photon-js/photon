cb02c528df40e40cc9a07d86f27d19013730048d
Joël Charles <joel.charles91@gmail.com>
2025-12-04 14:57:34 +0100

    chore: remove tests duplicates

--- example/app-netlify/tests/utils.ts
+++
@@ -1,7 +0,0 @@
-import type { Page } from "@playwright/test";
-
-export async function goto(page: Page, path: string) {
-  const response = await page.goto(path);
-  await page.waitForLoadState("networkidle");
-  return response;
-}
