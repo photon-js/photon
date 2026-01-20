7a8703be63bc8aa4c4523ee84b870781cc5ce969
Joël Charles <joel.charles91@gmail.com>
2026-01-20 15:20:48 +0100

    wip

--- packages/core/src/plugin/utils/escapeStringRegexp.ts
+++
@@ -1,4 +0,0 @@
-// Credit: https://github.com/sindresorhus/escape-string-regexp/blob/main/index.js
-export function escapeStringRegexp(subject: string) {
-  return subject.replace(/[|\\{}()[\]^$+*?.]/g, "\\$&").replace(/-/g, "\\x2d");
-}
