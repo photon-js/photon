ec8226e4f2ab34626e883393e60a9c055a05cd28
Joël Charles <joel.charles91@gmail.com>
2026-01-22 17:46:39 +0100

    cleanup

--- packages/runtime/src/vite.ts
+++ packages/runtime/src/vite.ts
@@ -76,39 +76,5 @@ export default mod;
         },
       },
     },
-    {
-      name: "photon:serve:transform-dev",
-      apply: "serve",
-
-      applyToEnvironment(env) {
-        return env.config.consumer === "server";
-      },
-
-      config(userConfig) {
-        userPort = userConfig.server?.port;
-        userHost = userConfig.server?.host;
-      },
-
-      transform: {
-        filter: {
-          code: [/process\.env\.PORT/, /process\.env\.PHOTON_HOSTNAME/],
-        },
-        handler(code) {
-          let newCode = code;
-          let replaced = false;
-          if (userPort) {
-            newCode = newCode.replaceAll("process.env.PORT", JSON.stringify(String(userPort)));
-            replaced = true;
-          }
-          if (typeof userHost === "string") {
-            newCode = newCode.replaceAll("process.env.PHOTON_HOSTNAME", JSON.stringify(String(userHost)));
-            replaced = true;
-          }
-          if (replaced) {
-            return newCode;
-          }
-        },
-      },
-    },
   ];
 }
