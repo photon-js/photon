98f6e30673af6059247702742dfcbe0d19522b2f
Joël Charles <joel.charles91@gmail.com>
2026-01-27 17:33:52 +0100

    tests: cloudflare

--- tests/app/tests/cloudflare/preview-fastify-cloudflare.test.ts
+++ tests/app/tests/cloudflare/preview-fastify-cloudflare.test.ts
@@ -1,3 +1,6 @@
 import { testRunUnsupported } from "../testRun.js";
 
-await testRunUnsupported("cloudflare", "preview", "fastify");
+await testRunUnsupported("cloudflare", "preview", "fastify", {
+  throwAtStart: true,
+  error: 'Unexpected Node.js imports for environment "ssr"',
+});
