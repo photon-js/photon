bd426cbe0d7c1f603b417510596c710886a3db0f
Joël Charles <joel.charles91@gmail.com>
2026-01-30 16:43:53 +0100

    tests: fix tanstack start example

--- example/tanstack-start/.test-preview.test.ts
+++ example/tanstack-start/.test-preview.test.ts
@@ -1,3 +1,12 @@
 import { testRun } from "./testRun.js";
 
-testRun("pnpm run preview --strictPort --port 3000");
+testRun("pnpm run preview --strictPort --port 3000", {
+  serverIsReadyMessage: "Listening on:",
+  tolerateError({ logText }) {
+    return [
+      // srvx messages
+      "Shutting down server",
+      "Server closed",
+    ].some((txt) => logText.includes(txt));
+  },
+});
