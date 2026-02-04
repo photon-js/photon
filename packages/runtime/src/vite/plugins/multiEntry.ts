f725ea97c59fab5c4563078b949f9ac7da9f1cb5
Joël Charles <joel.charles91@gmail.com>
2026-02-04 14:59:04 +0100

    chore: cleanup

--- packages/runtime/src/vite/plugins/multiEntry.ts
+++ packages/runtime/src/vite/plugins/multiEntry.ts
@@ -16,8 +16,6 @@ export function photonMultiEntryPlugin(options: PhotonPluginOptions): Plugin {
     name: "photon:resolve-entry",
     enforce: "pre",
     async config() {
-      // FIXME support .server.nodeHandler entries directly in VPV
-      // FIXME use same format as srvx for serve option and nodeHandler ?
       transformStoreInPlace(store, (entry) => ({
         ...entry,
         // ?γ is to avoid overriding the entry extension
